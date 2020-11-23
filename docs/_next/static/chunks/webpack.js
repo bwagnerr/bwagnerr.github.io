eb959ee2f7a9adf3db9e80f17e8bb88b06237158
Bruno Wagner Goncalves <b.wagner-goncalves@klarna.com>
2020-11-23 01:24:57 +0100

    Reverting es target to avoid issues

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4a7538d8125b15062969";
+/******/ 	var hotCurrentHash = "94b1b325b61d65ce4b1b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
